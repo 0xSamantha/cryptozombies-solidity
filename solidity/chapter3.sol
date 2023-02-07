0dbbd91d6c712bf4e5e35772ce804712d2bf41db
samcmcr <samchavoya@gmail.com>
2023-02-06 23:05:37 -0800

    state variables & integers

---
+++ solidity/chapter3.sol
@@ -0,0 +1,13 @@
+// Our Zombie DNA is going to be determined by a 16-digit number.
+
+// Declare a uint named dnaDigits, and set it equal to 16.
+
+
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    //start here
+
+}
+
