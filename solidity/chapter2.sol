6a6dfc30f22b43bc95ffa5a58ba01625850329c0
samcmcr <samchavoya@gmail.com>
2023-02-06 23:04:59 -0800

    contracts

---
+++ solidity/chapter2.sol
@@ -0,0 +1,14 @@
+/* to start creating our Zombie army, let's create a base contract called ZombieFactory.
+
+In the box to the right, make it so our contract uses solidity version >=0.5.0 <0.6.0.
+
+Create an empty contract called ZombieFactory.
+
+When you're finished, click "check answer" below. If you get stuck, you can click "hint".
+*/
+
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+}
